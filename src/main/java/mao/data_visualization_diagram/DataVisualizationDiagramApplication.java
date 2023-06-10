96ee02ee3065485d3f2f0ae7d0aa05c0f6285b2e
mao <1296193245@qq.com>
2023-06-10 22:03:35 +0800

    data_visualization_highcharts_diagram

---
+++ src/main/java/mao/data_visualization_diagram/DataVisualizationDiagramApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_diagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationDiagramApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationDiagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/data_visualization_diagram/DataVisualizationDiagramApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_diagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataVisualizationDiagramApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
